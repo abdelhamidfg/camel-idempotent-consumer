682125346500e2d96fae0496fb486e4f101a06a8
Abdelhamid Soliman <abdelhamidfg@gmail.com>
2022-12-21 23:15:58 +0400

    Create OrderProducer.java

---
+++ OrderProducer.java
@@ -0,0 +1,25 @@
+// camel-k: language=java 
+// camel-k: property-file=kafka.properties
+// camel-k: dependency=camel:http
+// camel-k: dependency=camel:gson
+// camel-k: dependency=mvn:javax.servlet:servlet-api:jar:2.5
+// camel-k: dependency=mvn:commons-logging:commons-logging:jar:1.2
+
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.model.dataformat.JsonLibrary;
+
+
+public class OrderProducer extends RouteBuilder {
+
+  @Override
+  public void configure() throws Exception {
+
+    
+    from("timer:java?period=90000")
+         .to("https://raw.githubusercontent.com/abdelhamidfg/greeting-service/master/orders.json")
+         .split().jsonpathWriteAsString("$.orders[*]")
+         .log(" ${body}")
+        .to("kafka:orders-duplicate-topic")
+        .log(" message has been sent to kafka topic");
+  }
+}
